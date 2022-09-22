3401c8af885899699d511c471508b2cfdfa1aaf7
yurysadounikau <114160200+yurysadounikau@users.noreply.github.com>
2022-09-22 18:42:09 +0300

    Add files via upload

---
+++ lab_3.1.2/lab_3.1.2.cpp
@@ -0,0 +1,33 @@
+
+#include <stdio.h>				3.1.2
+#include <locale.h>
+
+
+int main()
+{
+
+	setlocale(LC_ALL, "ru");
+
+	int n = 0;
+	printf("¬ведите число n:\n");
+	scanf_s(" %d", &n);
+	printf("\n");
+
+
+	int k = 0;
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j <= i; j++)
+		{
+			if (j * j * j + i * i * i == n)
+			{
+				k++;
+			}
+		}
+	}
+
+	printf("%d", k);
+
+
+	return 0;
+}
